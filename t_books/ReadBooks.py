46a0b6ccbf3197a07af8be0e4c5ae6fd4983b0a3
Gino Daza <155994323+GinoDazaU@users.noreply.github.com>
2024-11-08 23:56:02 -0500

    Update ReadBooks.py

--- t_books/ReadBooks.py
+++ t_books/ReadBooks.py
@@ -1,22 +1,34 @@
 import boto3
+import os
 import json
 from boto3.dynamodb.conditions import Key
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('t_books')
-
 def lambda_handler(event, context):
+    
     tenant_id = event['queryStringParameters']['tenant_id']
     page = int(event['queryStringParameters'].get('page', 1))
-    limit = 10  # Número de resultados por página
+    limit = 10
+    
+    nombre_tabla = os.environ["TABLE_NAME"]
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
 
+    start_key = None if page == 1 else {'tenant_id': tenant_id, 'isbn': event['queryStringParameters'].get('lastEvaluatedKey')}
+    
     response = table.query(
         KeyConditionExpression=Key('tenant_id').eq(tenant_id),
         Limit=limit,
-        ExclusiveStartKey={'tenant_id': tenant_id, 'isbn': response['LastEvaluatedKey']['isbn']} if page > 1 else None
+        ExclusiveStartKey=start_key
     )
-    
+
+    books = response.get('Items', [])
+    last_evaluated_key = response.get('LastEvaluatedKey', None)
+
     return {
         'statusCode': 200,
-        'body': json.dumps(response['Items'])
+        'body': json.dumps({
+            'books': books,
+            'lastEvaluatedKey': last_evaluated_key
+        })
     }
