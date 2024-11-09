014ec1d2ce1ffc633253b83a84c1a744c13a8540
Gino Daza <155994323+GinoDazaU@users.noreply.github.com>
2024-11-09 00:03:59 -0500

    Update ReadBooks.py

--- t_books/ReadBooks.py
+++ t_books/ReadBooks.py
@@ -4,18 +4,25 @@ import json
 from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
-    
     tenant_id = event['queryStringParameters']['tenant_id']
     page = int(event['queryStringParameters'].get('page', 1))
-    limit = 10
+    limit = 10  # Número de resultados por página
     
+    # Obtener el nombre de la tabla desde la variable de entorno
     nombre_tabla = os.environ["TABLE_NAME"]
     
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
 
-    start_key = None if page == 1 else {'tenant_id': tenant_id, 'isbn': event['queryStringParameters'].get('lastEvaluatedKey')}
+    # Usar lastEvaluatedKey si se proporciona y no es la primera página
+    start_key = None
+    if page > 1 and 'lastEvaluatedKey' in event['queryStringParameters']:
+        start_key = {
+            'tenant_id': tenant_id,
+            'isbn': event['queryStringParameters']['lastEvaluatedKey']
+        }
     
+    # Consulta con paginación
     response = table.query(
         KeyConditionExpression=Key('tenant_id').eq(tenant_id),
         Limit=limit,
